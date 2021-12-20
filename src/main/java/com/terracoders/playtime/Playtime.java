21eacf20880e3767953f846ce8d1454f66a7cec7
David Diaz <services@terracoders.support>
2021-12-20 14:48:58 -0700

    'general updates'

--- src/main/java/com/terracoders/playtime/Playtime.java
+++ src/main/java/com/terracoders/playtime/Playtime.java
@@ -2,7 +2,8 @@ package com.terracoders.playtime;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Playtime extends JavaPlugin {
@@ -14,13 +15,16 @@ public final class Playtime extends JavaPlugin {
 	public void onDisable() {
 		getLogger().info("Playtime Plugin has been disabled");
 	}
-	public void onPlayerInteractBlock(PlayerInteractEvent event) {
-	    Player player = event.getPlayer();
-	    if (player.getItemInHand().getType() == Material.BONE) {
-	        // Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
-	        // Can only create lightning up to 200 blocks away.
-	        player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
-	    }
+
+	public void onPlayerJoinEvent(Player playerJoined) {
+		//DIAMOND CHEAT -- IF USER HOLDING DIAMOND MATERIAL OBJECT IN MAIN HAND
+		//ALL ARMOR BECOMES DIAMOND ARMOR
+	    Player player = playerJoined.getPlayer();
+	    PlayerInventory inventory = player.getInventory();
+	    ItemStack itemstack = new ItemStack(Material.DIAMOND, 64);
+	    inventory.addItem(itemstack);
+	    player.sendMessage("Diamonds for your journey!");
+
 	}
 
 
